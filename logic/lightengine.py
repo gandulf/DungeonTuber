f1e20a1cfdd03aae5010d50380e205745f3e932a
Gandulf Kohlweiss <gandulf.kohlweiss@ilogs.com>
2026-04-21 14:18:57 +0200

    Add option to mack bulbs for easier testing

--- logic/lightengine.py
+++ logic/lightengine.py
@@ -3,17 +3,17 @@ import atexit
 
 import json
 import logging
-
+import random
 from dataclasses import dataclass, field
 from functools import partial
-from typing import List
+from typing import List, Optional
 
 from PySide6.QtCore import QThread, Signal, QObject
 from PySide6.QtGui import QColor
-from pywizlight import wizlight, PilotBuilder, discovery
+from pywizlight import wizlight, PilotBuilder, PilotParser, BulbType
 from pywizlight.discovery import DEFAULT_WAIT_TIME, BroadcastProtocol, PORT
 from pywizlight.models import DiscoveredBulb, BulbRegistry
-from pywizlight.scenes import get_id_from_scene_name
+from pywizlight.scenes import get_id_from_scene_name, SCENES
 from pywizlight.utils import create_udp_broadcast_socket
 
 from config.settings import SettingKeys, AppSettings
@@ -37,7 +37,6 @@ def get_lights():
 
 
 def save_on_exit():
-    print(f"Storing{get_lights()}")
     AppSettings.setValue(SettingKeys.LIGHTS_CONFIG, Light.json_dump_list(get_lights()))
 
 
@@ -57,7 +56,7 @@ class LightSetting:
 
 @dataclass
 class Light:
-    name: str = "Licht"
+    name: str = "Light"
     scenable: bool = True
     _scene: str | None = None
 
@@ -85,8 +84,6 @@ class Light:
         self._temperature = _temperature
         self._color = _color
         self._scene = _scene
-
-        self.loop = asyncio.get_event_loop()
 
     def apply_settings(self, settings: LightSetting):
         if self.scene is not None:
@@ -213,6 +210,8 @@ class Light:
         await self.control.turn_off()
 
     def _execute(self, func):
+        self.loop = asyncio.get_event_loop()
+
         if self.loop.is_running():
             return asyncio.run_coroutine_threadsafe(func(), self.loop).result(5)
         else:
@@ -284,6 +283,7 @@ class LightManager(QObject):
 
 
 class DiscoveryThread(QThread):
+    FAKE_BULBS = False
     """
     A dedicated thread to run the asyncio event loop.
     """
@@ -295,18 +295,28 @@ class DiscoveryThread(QThread):
 
     def run(self):
 
-        broadcast_adress = AppSettings.value(SettingKeys.LIGHTS_BROADCAST_IP, get_broadcast_ip(), type=str)
-        timeout = AppSettings.value(SettingKeys.LIGHTS_TIMEOUT, 5, type=float)
-        # 2. Run your async tasks until complete (or forever)
-        logger.info("Start lookup with broadcast address: %s for %s seconds", broadcast_adress, timeout)
-        # Use wait_for to ensure the thread doesn't hang forever
-        bulbs = self.loop.run_until_complete(discover_lights(broadcast_space=broadcast_adress, wait_time=timeout))
-        # Run the discovery coroutine
+        if self.FAKE_BULBS:
+            bulbs = []
+            for n in range(3):
+                control = MockControl()
+                control.mac = f"AA:BB:CC:DD:EE:F{n}"
+                bulbs.append(control)
 
-        lights = self.loop.run_until_complete(self.update_states(bulbs))
-        self.bulbs_found.emit(lights)
+            lights = self.loop.run_until_complete(self.update_states(bulbs))
+            self.bulbs_found.emit(lights)
+        else:
+            broadcast_address = AppSettings.value(SettingKeys.LIGHTS_BROADCAST_IP, get_broadcast_ip(), type=str)
+            timeout = AppSettings.value(SettingKeys.LIGHTS_TIMEOUT, 5, type=float)
+            # 2. Run your async tasks until complete (or forever)
+            logger.info("Start lookup with broadcast address: %s for %s seconds", broadcast_address, timeout)
+            # Use wait_for to ensure the thread doesn't hang forever
+            bulbs = self.loop.run_until_complete(discover_lights(broadcast_space=broadcast_address, wait_time=timeout))
+            # Run the discovery coroutine
 
-    async def update_states(self, bulbs: list[wizlight]):
+            lights = self.loop.run_until_complete(self.update_states(bulbs))
+            self.bulbs_found.emit(lights)
+
+    async def update_states(self, bulbs: list[wizlight]) -> list[Light]:
         lights = []
         for bulb in bulbs:
             light = None
@@ -358,4 +368,90 @@ async def discover_lights(
     return [
         wizlight(ip=entry.ip_address, mac=entry.mac_address) for entry in discovered_IPs
     ]
+
+
 ## wizlight BUGFIX END
+
+
+class MockControl:
+    mac: str = "AA:BB:CC:DD:EE"
+
+    state: bool = True
+    r: int = 250
+    g: int = 0
+    b: int = 0
+    temp: int = 4000
+    c: int = 128
+    w: int = 128
+    scene_id: int = None
+    dimming: int = 100
+
+    def __init__(self, ):
+        self.mac = self.generate_mac()
+
+    def generate_mac(self):
+        # Generate 6 random bytes
+        mac = [random.randint(0x00, 0xff) for _ in range(6)]
+
+        # Format as hex strings: 00:11:22:33:44:55
+        return ":".join(f"{b:02x}" for b in mac)
+
+    async def updateState(self) -> Optional[PilotParser]:
+        fakeResponse = {
+            "state": self.state,
+            "src": "127.0.0.1",
+            "mac": self.mac,
+            "pc": 1000,
+            "w": self.w,
+            "whiteRange": [1000, 3000],
+            "extRange": [2200, 2700, 6500, 6500],
+            "speed": 100,
+            "ratio": 10,
+            "sceneId": self.scene_id,
+            "c": self.c,
+            "dimming": self.dimming,
+            "temp": self.temp,
+
+            "r": self.r,
+            "g": self.g,
+            "b": self.b
+        }
+        return PilotParser(fakeResponse)
+
+    async def get_bulbtype(self) -> BulbType:
+        return BulbType.from_data(module_name="MockBulb_RGB", type_id=0, kelvin_list=[2000, 3500, 4500, 6500],
+                                  fw_version="0.0.1", fan_speed_range=10, white_channels=2, white_to_color_ratio=5)
+
+    async def getSupportedScenes(self) -> list[str]:
+        return SCENES.values()
+
+    async def turn_on(self, state: PilotBuilder):
+        if "sceneId" in state.pilot_params:
+            self.scene_id = state.pilot_params["sceneId"]
+        if "r" in state.pilot_params:
+            self.r = state.pilot_params["r"]
+        if "g" in state.pilot_params:
+            self.g = state.pilot_params["g"]
+
+        if "b" in state.pilot_params:
+            self.b = state.pilot_params["b"]
+
+        if "state" in state.pilot_params:
+            self.state = state.pilot_params["state"]
+
+        if "temp" in state.pilot_params:
+            self.temp = state.pilot_params["temp"]
+
+        if "c" in state.pilot_params:
+            self.c = state.pilot_params["c"]
+
+        if "w" in state.pilot_params:
+            self.w = state.pilot_params["w"]
+
+        if "dimming" in state.pilot_params:
+            self.dimming = state.pilot_params["dimming"]
+        print("turn on with %s" % state.pilot_params)
+
+    async def turn_off(self):
+        self.state = False
+        print("turn off")
